943af48f7f2327ef40fc8f737b4875e0c7e808fc
emoose <abc@cock.li>
2024-04-29 08:54:27 +0100

    yaml: add main/bodyprog symbols to overlays

---
+++ src/stream/overlay_stream.c
@@ -0,0 +1,31 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", func_801E2654);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", func_801E279C);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", func_801E2838);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", func_801E28B0);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", func_801E2908);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", func_801E2A24);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", open_main);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", movie_main);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", strSetDefDecEnv);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", strInit);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", strCallback);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", strKickCD);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", strNextVlc);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", strNext);
+
+INCLUDE_ASM("asm/stream/nonmatchings/overlay_stream", strSync);
