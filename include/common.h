d28400915520ce28ea847b166fda6745a95633ca
emoose <abc@cock.li>
2024-05-09 02:03:39 +0100

    psxsdk: add libgs and libgte

--- include/common.h
+++ include/common.h
@@ -4,10 +4,11 @@
 #include "include_asm.h"
 #include "types.h"
 
+#include <psxsdk/libgte.h>
 #include <psxsdk/libcd.h>
 #include <psxsdk/libetc.h>
-#include <psxsdk/libgte.h>
 #include <psxsdk/libgpu.h>
+#include <psxsdk/libgs.h>
 #include <psxsdk/libpress.h>
 #include <psxsdk/libsnd.h>
 
