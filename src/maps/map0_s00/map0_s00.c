859b27c138a9d883f9d640649cfdc419d98fcdf8
emoose <abc@cock.li>
2025-09-23 22:14:30 +0100

    bodyprog: use e_PrimitiveType

--- src/maps/map0_s00/map0_s00.c
+++ src/maps/map0_s00/map0_s00.c
@@ -1054,7 +1054,7 @@ void func_800DB514(void)
             func_80085EB8(1, &g_SysWork.player_4C.chara_0, 0, false);
             break;
         case 9:
-            func_8003EF10(10, 10, 5, &D_800DFB5C, 0, Q12(0.5f));
+            func_8003EF10(10, 10, PrimitiveType_S32, &D_800DFB5C, 0, Q12(0.5f));
             SysWork_StateStepIncrement();
 
             /* fallthrough */
@@ -1439,7 +1439,7 @@ void func_800DCC54(void) // 0x800DCC54
 {
     if (D_800DFADC == NO_VALUE)
     {
-        func_8003EF10(6, 6, 5, &D_800DFADC, Q12(0.0f), Q12(100.0f));
+        func_8003EF10(6, 6, PrimitiveType_S32, &D_800DFADC, Q12(0.0f), Q12(100.0f));
         D_800DFADC = Q12(0.0f);
 
         Sd_PlaySfx(Sfx_Unk1359, 0, Q8_CLAMPED(0.875f));
@@ -1555,7 +1555,7 @@ void func_800DD0CC()
 
     if (D_800DFB40 == NO_VALUE)
     {
-        func_8003EF10(6, 6, 5, &D_800DFB40, 0, Q12(100.0f));
+        func_8003EF10(6, 6, PrimitiveType_S32, &D_800DFB40, 0, Q12(100.0f));
         D_800DFB40 = 0;
     }
 
